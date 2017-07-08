62f9920ac6f2defd932bdc19e898ffac6e99ede7
maramkura <niordanishvili@yahoo.it>
2017-07-08 13:27:56 +0400

    Auth, Admin and db seed created

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class UsersTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
+         DB::table('users')->insert([
             'name' => str_random(5),
             'email' => str_random(5).'@admin.com',
             'password' => bcrypt('secret'),
